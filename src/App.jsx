abf442eca5478fdeed7b7754e654ae4822280bf1
JSWww <tjddnjs9497@naver.com>
2021-07-27 16:39:09 +0900

    feat: global style 추가

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import InfiniteScrollList from './components/InfiniteScrollList';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <InfiniteScrollList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Courier New', Courier, monospace;
+    background-color: #f2f2f2;
+  }
+`;
+
+export default GlobalStyle;
